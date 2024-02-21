1704749aa1a4667f963687084862dc0469dbe65a
Grace Goheen <grace.goheen@dbtlabs.com>
2024-02-21 16:10:30 -0700

    add unit test example 1

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -8,11 +8,16 @@ source as (
 
 staged as (
 
-    select
+    select 
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status like '%pending%' as is_status_pending,
+        case 
+            when status like '%return%' then 'returned'
+            when status like '%pending%' then 'placed'
+            else status
+        end as status
     from source
 
 )

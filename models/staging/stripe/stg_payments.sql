42fd5da7e48a4f590210a4b1013e608c751195a3
Grace Goheen <grace.goheen@dbtlabs.com>
2024-02-21 15:28:44 -0700

    add simple unit test

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -14,7 +14,7 @@ staged as (
         paymentmethod as payment_method,
         status,
         -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
+        round(amount / 100, 2) as amount,
         created as created_at
     from source
 
